3b5bad2e7ec3a801cb64aa76a579130d1c9faf76
Sierra Jo Sallee <sierrajosallee@gmail.com>
2020-05-28 06:55:00 -0500

    Init part 9

---
+++ C# and .NET - Programming Course/Ex_Files_Csharpnet/Exercise Files/Ch02/02_02/LearningLists/LearningLists/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> customers = new List<string>();
+
+            customers.Add("Kim");
+            customers.Add("John");
+            customers.Add("Tim");
+
+            Console.WriteLine(customers.Count);
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer);
+            }
+
+            Console.WriteLine(customers[1]);
+            Console.Read();
+        }
+    }
+}
